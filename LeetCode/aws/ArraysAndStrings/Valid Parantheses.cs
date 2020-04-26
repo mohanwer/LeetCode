da847796dc94444cc6d62e79c137428d450003b4
mohanwer <mohrocks4@gmail.com>
2020-04-25 21:20:48 -0400

    Completed question.

---
+++ LeetCode/aws/ArraysAndStrings/Valid Parantheses.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace LeetCode.aws.ArraysAndStrings
+{
+    //https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/2973/
+    public partial class Solution
+    {
+        public bool IsOpener(char character) =>
+            character == '[' || character == '(' || character == '{';
+
+        public bool IsCloser(char character) =>
+            character == ']' || character == ')' || character == '}';
+
+        public bool IsValidCloser(char opener, char closer) => 
+            opener switch
+            {
+                '[' when closer == ']' => true,
+                '{' when closer == '}' => true,
+                '(' when closer == ')' => true,
+                _ => false
+            };
+        
+        public bool IsValid(string s)
+        {
+            var parans = new Stack<char>();
+            foreach (var character in s)
+            {
+                if (IsOpener(character))
+                    parans.Push(character);
+                else if (IsCloser(character))
+                {
+                    if (parans.Count == 0) return false; 
+                    var openedLast = parans.Pop();
+                    if (!IsValidCloser(openedLast, character))
+                        return false;
+                }
+            }
+            
+            return parans.Count == 0;
+        }
+
+        [Fact]
+        public void TestIsValid()
+        {
+            Assert.False(IsValid("([)]"));
+        }
+    }
+}
